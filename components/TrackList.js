99eee3e3b834d58d3a69ab62b65bf0a2ace7668a
Anthony Ferraro <adferra24@gmail.com>
2021-12-09 23:28:41 -0500

    started reworking tracklist layout

--- components/TrackList.js
+++ components/TrackList.js
@@ -2,6 +2,13 @@ import styles from ".././styles/TrackInfo.module.css"
 import Image from "next/image"
 
 const TrackList = ({ tracks }) => {
+  console.log(tracks[4])
+  const returnTime = (ms) => {
+    const rd = Math.round(100 * (ms / 1000 / 60)) / 100
+    const wh = Math.floor(rd) + ""
+    const th = Math.round(100 * (rd - Math.floor(rd))) + ""
+    return wh + ":" + th
+  }
   return (
     <div className={styles.tracklist__container}>
       <div className={styles.tracklist__grid}>
@@ -18,14 +25,29 @@ const TrackList = ({ tracks }) => {
         <span
           className={`${styles.tracklist__cell} ${styles.tracklist__cellHeader}`}
         >
-          Artist
+          Album
+        </span>
+        <span
+          className={`${styles.tracklist__cell} ${styles.tracklist__cellHeader}`}
+        >
+          Date Added
+        </span>
+        <span
+          className={`${styles.tracklist__cell} ${styles.tracklist__cellHeader}`}
+          style={{ justifySelf: "end" }}
+        >
+          Time
         </span>
       </div>
 
       {tracks.map((track, index) => {
         return (
           <div className={styles.tracklist__grid} key={index}>
-            <span className={styles.tracklist__cell}>{index} </span>
+            <span
+              className={`${styles.tracklist__cell} ${styles.tracklist__cellOPaque}`}
+            >
+              {index + 1}{" "}
+            </span>
             <div className={styles.tracklist__cell}>
               <div className={styles.track__image}>
                 <Image
@@ -41,10 +63,39 @@ const TrackList = ({ tracks }) => {
                   className={styles.playlist__image}
                 />{" "}
               </div>
-              <span className={styles.track__name}>{track.name}</span>
+              <div className={styles.track__artistContainer}>
+                <span className={styles.track__name}>{track.name}</span>
+                <span
+                  className={`${styles.track__artist} ${styles.tracklist__cellOPaque}`}
+                >
+                  {track.artists.map((e, i) => {
+                    if (
+                      i === track.artists.length - 1 ||
+                      track.artists.length === 1
+                    ) {
+                      return e.name
+                    } else {
+                      return `${e.name}, `
+                    }
+                  })}
+                </span>
+              </div>
             </div>
-            <span className={styles.tracklist__cell}>
-              {track.artists[0].name}
+            <span
+              className={`${styles.tracklist__cell} ${styles.tracklist__cellOPaque}`}
+            >
+              {track.album.name}
+            </span>
+            <span
+              className={`${styles.tracklist__cell} ${styles.tracklist__cellOPaque}`}
+            >
+              {track.album.name}
+            </span>
+            <span
+              className={`${styles.tracklist__cell} ${styles.tracklist__cellOPaque}`}
+              style={{ justifySelf: "end" }}
+            >
+              {returnTime(track.duration_ms)}
             </span>
           </div>
         )
