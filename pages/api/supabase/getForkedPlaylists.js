be63509a933ce612b399716a789a5ad3bca83e83
Anthony Ferraro <adferra24@gmail.com>
2021-12-16 23:04:39 -0500

    fixed refreshing a forked playlist

--- pages/api/supabase/getForkedPlaylists.js
+++ pages/api/supabase/getForkedPlaylists.js
@@ -1,17 +1,27 @@
 import { supabase } from "../../../lib/supabase/client"
 
 export default async (req, res) => {
-  const { id } = req.query
+  const { id, playlist_id } = req.query
+  let resp = {}
   try {
-    const { data, error } = await supabase
-      .from("forked_playlists")
-      .select("playlist_id, master_playlist_id, playlist")
-      .eq("spotify_id", id)
-    if (error) throw error
+    if (!playlist_id) {
+      resp = await supabase
+        .from("forked_playlists")
+        .select("playlist_id, master_playlist_id, playlist")
+        .eq("spotify_id", id)
+      if (resp.error) throw resp.error
+    } else {
+      resp = await supabase
+        .from("forked_playlists")
+        .select("playlist_id, master_playlist_id, playlist")
+        .match({ spotify_id: id, playlist_id: playlist_id })
 
-    res.status(200).json(data)
+      if (resp.error) throw resp.error
+    }
+
+    res.status(200).json(resp.data)
   } catch (error) {
-    console.error(error)
-    res.status(400).json(error)
+    console.error(error.message)
+    res.status(400).json({ error: error.message })
   }
 }
--- pages/playlists/[playlistId].js
+++ pages/playlists/[playlistId].js
@@ -55,22 +55,38 @@ const playlists = ({ usr, propSession }) => {
       if (!session) return
       if (!playlist.playlistId) {
         const { playlistId } = router.query
-        const getPlaylistRes = await fetch(
-          `/api/spotify/getPlaylist?playlist_id=${playlistId}&access_token=${session.access_token}`
-        )
 
-        const playlistObj = await getPlaylistRes.json()
-        const playlist = {
-          name: playlistObj.name,
-          playlistId: playlistObj.id,
-          trackCount: playlistObj.tracks,
-          trackTotal: playlistObj.tracks.total,
-          reqCount: Math.round(playlistObj.tracks.total / 100 + 0.5),
-          owner: playlistObj.owner,
-          image: playlistObj?.images[0]?.url,
-          description: playlistObj.description,
+        try {
+          const getForkedPlaylistsRes = await fetch(
+            `/api/supabase/getForkedPlaylists?id=${user.id}&playlist_id=${playlistId}`
+          )
+          const forkedPlaylist = await getForkedPlaylistsRes.json()
+          if (getForkedPlaylistsRes.status === 400) {
+            throw new Error(forkedPlaylist.error)
+          }
+          if (forkedPlaylist.length > 0) {
+            handleSetPlaylist({ ...forkedPlaylist[0].playlist, isFork: true })
+          } else {
+            const getPlaylistRes = await fetch(
+              `/api/spotify/getPlaylist?playlist_id=${playlistId}&access_token=${session.access_token}`
+            )
+
+            const playlistObj = await getPlaylistRes.json()
+            const playlist = {
+              name: playlistObj.name,
+              playlistId: playlistObj.id,
+              trackCount: playlistObj.tracks,
+              trackTotal: playlistObj.tracks.total,
+              reqCount: Math.round(playlistObj.tracks.total / 100 + 0.5),
+              owner: playlistObj.owner,
+              image: playlistObj?.images[0]?.url,
+              description: playlistObj.description,
+            }
+            handleSetPlaylist(playlist)
+          }
+        } catch (error) {
+          console.error(error)
         }
-        handleSetPlaylist(playlist)
       }
     })()
   }, [session])
