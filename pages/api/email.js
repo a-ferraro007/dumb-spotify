a3d9296890431869814b657e20e4a05c4c781cbd
Anthony <adferra24@gmail.com>
2022-09-02 09:40:10 -0400

    swap sendgrid for mailjet

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,18 +1,12 @@
-import sgMail from "@sendgrid/mail"
+const Mailjet = require("node-mailjet")
 import { supabase } from "../../lib/supabase/client"
-sgMail.setApiKey(process.env.SEND_GRID)
+const mailjet = new Mailjet({
+  apiKey: process.env.MJ_APIKEY_PUBLIC,
+  apiSecret: process.env.MJ_APIKEY_PRIVATE,
+})
 
 export default async (req, res) => {
   const { email } = req.body
-
-  const msg = {
-    to: email,
-    from: "really@dumbspotify.com",
-    subject: "You really signed up for this? Yikes.",
-    text: "Welcome to the dumb spotify waitlist, I'll email you if I ever finish...",
-    html: `<strong>Welcome to the dumb spotify waitlist!</strong> <br>
-    <span>We'll email you again if we ever release this thing...  </span>`,
-  }
   try {
     const { data, error } = await supabase.from("beta_signups").insert([
       {
@@ -23,7 +17,28 @@ export default async (req, res) => {
       throw error
     }
 
-    await sgMail.send(msg)
+    await mailjet.post("send", { version: "v3.1" }).request({
+      Messages: [
+        {
+          From: {
+            Email: "really@dumbspotify.com",
+            Name: "Gerald",
+          },
+          To: [
+            {
+              Email: `${email}`,
+            },
+          ],
+          Subject: "You really signed up for this? Yikes.",
+          TextPart:
+            "Welcome to the dumb spotify waitlist, I'll email you if I ever finish...",
+          HTMLPart: `<strong>Welcome to the dumb spotify waitlist!</strong> <br>
+            <span>We'll email you again if we ever release this thing...  </span>
+            <br>
+            <span> please dont sue me. </span>`,
+        },
+      ],
+    })
     res.status(200).json(data)
   } catch (error) {
     console.error(error)
